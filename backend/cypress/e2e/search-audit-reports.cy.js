de9fe7c39786d000cce0ad76b29735bc37395481
Edward Zapata <edward.zapata@gsa.gov>
2024-02-27 17:14:19 -0500

    add cypress search audit test to e2e & support
    Signed-off-by: Edward Zapata <99770283+eazapata23@users.noreply.github.com>

---
+++ backend/cypress/e2e/search-audit-reports.cy.js
@@ -0,0 +1,7 @@
+import { testSearch } from '../support/search-audit-reports.js';
+
+    describe('Test Search Page', () => {
+        it('Display the search page', () => {
+          testSearch();
+        });
+      });
---
+++ backend/cypress/support/search-audit-reports.js
@@ -0,0 +1,31 @@
+
+const yearsToCheck = ['2023','2022','2021','2020','2019','2018','2017','2016'];
+
+export function testSearch(){
+    cy.visit('/dissemination/search/');
+    //cy.get('#audit-year-2023').check({force: true}).should('be.checked').and('have.value','2023');
+   // cy.get('#audit-year-2023').uncheck({force: true}).should('not.be.checked');
+    //cy.get('input[type="checkbox"]').check(['2022','2021']);
+    cy.get('input[type="checkbox"]').each((checkbox, index) => {
+        const year = yearsToCheck[index];
+        cy.wrap(checkbox).check({ force: true}).should('have.value',year);
+        //cy.get('#uei-or-ein').clear().type('D7A4J33FUMJ1');
+    });
+
+    cy.get('#uei-or-ein').clear().type('D7A4J33FUMJ1');
+    cy.get('#aln').clear().type('93');
+    cy.get('#entity-name').clear().type('Some Important Auditee Firm');
+
+    cy.get('#start-date').type('02/01/2024');
+    cy.get('#end-date').type('02/28/2024');
+
+    cy.get('#options').select('oversight').should('have.value','oversight');
+
+    cy.get('#agency-name').type('Over-93');
+
+    cy.get('#auditee_state').select('VA').should('have.value','VA');
+
+    cy.get(':nth-child(4) > [type="submit"]').should('have.value','Search').click();
+    cy.url().should('match', /\/dissemination\/search\//);
+
+}
